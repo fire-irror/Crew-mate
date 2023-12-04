cb4ddc666312fc1bd921ef16431be84b1d71f6a3
dinmoy <imhyojin614@gmail.com>
2023-12-04 13:20:22 +0900

    [Feat] 로그인시 alert창 띄우기

--- serverConn/join.js
+++ serverConn/join.js
@@ -7,21 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const Id = document.getElementById('username');
         const Password = document.getElementById('password');
+        const Email = document.getElementById('email');
 
         let id = Id.value;
         let password = Password.value;
+        let email = Email.value;
 
         let userData = {
             user_name: id,
-            password: password
+            password: password,
+            email: email
         };
-
-        axios.post('http://localhost:3000/api/users/login', userData)
+        axios.post('http://localhost:3000/api/users/join', userData)
             .then(res => {
-                console.log('로그인 성공:', res.data); 
+                console.log('회원가입 성공:', res.data); 
+                document.addEventListener('DOMContentLoaded', () => {
+    const submitBtn = document.getElementById('submit');
+    const registrationForm = document.getElementById('registrationForm');
+
+    registrationForm.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+
+        const Id = document.getElementById('username');
+        const Password = document.getElementById('password');
+        const Email = document.getElementById('email');
+
+        let id = Id.value;
+        let password = Password.value;
+        let email = Email.value;
+
+        let userData = {
+            user_name: id,
+            password: password,
+            email: email
+        };
+        axios.post('http://localhost:3000/api/users/join', userData)
+            .then(res => {
+                console.log('회원가입 성공:', res.data); 
+                window.location.href = '로그인.html';
             })
             .catch(error => {
-                console.error('로그인 실패:', error); 
+                console.error('회원가입 실패:', error); 
+        
+            });
+    });
+});
+            })
+            .catch(error => {
+                console.error('회원가입 실패:', error); 
+        
             });
     });
 });
--- serverConn/login.js
+++ serverConn/login.js
@@ -19,9 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
         axios.post('http://localhost:3000/api/users/login', userData)
             .then(res => {
                 console.log('로그인 성공:', res.data); 
+                alert("로그인 성공");
+                window.location.href = 'index.html'; // 로그인 성공 시 처음페이지로
             })
             .catch(error => {
                 console.error('로그인 실패:', error); 
+                alert("로그인 실패");
+                window.location.href = '로그인.html';
             });
     });
 });
